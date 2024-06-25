506a68fccfe22a1c3176f1b81beaa86890b5e1e3
lkportal <lksxim@gmail.com>
2024-06-25 14:42:37 -0300

    add bd sqlserver and method login

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -23,22 +23,172 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.SuspendLayout();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnCriar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtIdentificadorCadastro = new System.Windows.Forms.TextBox();
+            this.txtNomeCadastro = new System.Windows.Forms.TextBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.txtIdentificadoLogin = new System.Windows.Forms.TextBox();
+            this.txtNomeLogin = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
-            // splitContainer1
+            // panel1
             // 
-            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-            this.splitContainer1.IsSplitterFixed = true;
-            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
-            this.splitContainer1.Name = "splitContainer1";
-            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
-            this.splitContainer1.SplitterDistance = 224;
-            this.splitContainer1.SplitterIncrement = 2;
-            this.splitContainer1.TabIndex = 0;
+            this.panel1.Controls.Add(this.btnCriar);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.txtIdentificadorCadastro);
+            this.panel1.Controls.Add(this.txtNomeCadastro);
+            this.panel1.Location = new System.Drawing.Point(473, 36);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(269, 338);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnCriar
+            // 
+            this.btnCriar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCriar.Location = new System.Drawing.Point(101, 257);
+            this.btnCriar.Name = "btnCriar";
+            this.btnCriar.Size = new System.Drawing.Size(75, 30);
+            this.btnCriar.TabIndex = 8;
+            this.btnCriar.Text = "Criar";
+            this.btnCriar.UseVisualStyleBackColor = true;
+            this.btnCriar.Click += new System.EventHandler(this.btnCriar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(76, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(115, 25);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Cadastrar";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 169);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(110, 20);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Identificador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Nome";
+            // 
+            // txtIdentificadorCadastro
+            // 
+            this.txtIdentificadorCadastro.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdentificadorCadastro.Location = new System.Drawing.Point(3, 192);
+            this.txtIdentificadorCadastro.Name = "txtIdentificadorCadastro";
+            this.txtIdentificadorCadastro.Size = new System.Drawing.Size(263, 29);
+            this.txtIdentificadorCadastro.TabIndex = 4;
+            // 
+            // txtNomeCadastro
+            // 
+            this.txtNomeCadastro.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeCadastro.Location = new System.Drawing.Point(3, 128);
+            this.txtNomeCadastro.Name = "txtNomeCadastro";
+            this.txtNomeCadastro.Size = new System.Drawing.Size(263, 29);
+            this.txtNomeCadastro.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.btnEntrar);
+            this.panel2.Controls.Add(this.txtIdentificadoLogin);
+            this.panel2.Controls.Add(this.txtNomeLogin);
+            this.panel2.Location = new System.Drawing.Point(67, 36);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(269, 338);
+            this.panel2.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Identificador";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(95, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 25);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Login";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nome";
+            // 
+            // btnEntrar
+            // 
+            this.btnEntrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEntrar.Location = new System.Drawing.Point(92, 257);
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.Size = new System.Drawing.Size(75, 30);
+            this.btnEntrar.TabIndex = 2;
+            this.btnEntrar.Text = "Entrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            this.btnEntrar.Click += new System.EventHandler(this.btnEntrar_Click);
+            // 
+            // txtIdentificadoLogin
+            // 
+            this.txtIdentificadoLogin.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIdentificadoLogin.Location = new System.Drawing.Point(3, 192);
+            this.txtIdentificadoLogin.Name = "txtIdentificadoLogin";
+            this.txtIdentificadoLogin.Size = new System.Drawing.Size(263, 29);
+            this.txtIdentificadoLogin.TabIndex = 1;
+            // 
+            // txtNomeLogin
+            // 
+            this.txtNomeLogin.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNomeLogin.Location = new System.Drawing.Point(3, 128);
+            this.txtNomeLogin.Name = "txtNomeLogin";
+            this.txtNomeLogin.Size = new System.Drawing.Size(263, 29);
+            this.txtNomeLogin.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(24, 406);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(764, 35);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "label7";
             // 
             // Form1
             // 
@@ -46,18 +196,36 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkSeaGreen;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Home";
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtIdentificadorCadastro;
+        private System.Windows.Forms.TextBox txtNomeCadastro;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.TextBox txtIdentificadoLogin;
+        private System.Windows.Forms.TextBox txtNomeLogin;
+        private System.Windows.Forms.Button btnCriar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label7;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,11 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.IO;
+using System.Threading;
 
 namespace InglesFerramenta {
     public partial class Form1 : Form {
+
+        SqlConnection connection;
+        SqlCommand command;
+        SqlDataAdapter adapter; 
+
         public Form1() {
             InitializeComponent();
+                connection = new SqlConnection(Environment.GetEnvironmentVariable("HKEY_CURRENT_USER\\Environment", EnvironmentVariableTarget.User));
+                command = new SqlCommand();
+                adapter = new SqlDataAdapter();
+        }
+
+        private void btnCriar_Click(object sender, EventArgs e) {
+            try {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "INSERT INTO USUARIO (NOME,IDETIFICADOR) VALUES (@NOME,@IDETIFICADOR)";
+                if(!string.IsNullOrEmpty(txtNomeCadastro.Text) && !string.IsNullOrEmpty(txtIdentificadorCadastro.Text)) {
+                    command.Parameters.AddWithValue("@NOME", txtNomeCadastro.Text.ToLower());
+                    command.Parameters.AddWithValue("@IDETIFICADOR", txtIdentificadorCadastro.Text.ToLower());
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Usuário Adicionado", "Adicionado");
+                }
+                else {
+                    MessageBox.Show("Preencha os campos vazios","Avviso");
+                }
+            
+
+            } catch(SqlException ex) {
+                MessageBox.Show(ex.Message, "Erro SQL");
+            } finally {
+                connection.Close();
+                command.Dispose();
+                txtIdentificadorCadastro.Text = "";
+                txtNomeCadastro.Text = "";
+            }
+
+
+
+        }
+
+        private void btnEntrar_Click(object sender, EventArgs e) {
+            try {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT NOME,IDETIFICADOR FROM USUARIO WHERE NOME = @NOME AND IDETIFICADOR = @COD";
+                if(!string.IsNullOrEmpty(txtNomeLogin.Text) && !string.IsNullOrEmpty(txtIdentificadoLogin.Text)) {
+                    command.Parameters.AddWithValue("@NOME", txtNomeLogin.Text);
+                    command.Parameters.AddWithValue("@COD", txtIdentificadoLogin.Text);
+                    command.ExecuteNonQuery();
+                    adapter.SelectCommand = command;
+                }
+                DataTable table =  new DataTable();
+                adapter.Fill(table);
+
+                foreach(DataRow row in table.Rows) {
+                    if (row["Nome"].ToString() == txtNomeLogin.Text.ToLower() && row["IDETIFICADOR"].ToString() == txtIdentificadoLogin.Text.ToLower())  {
+                        Close();
+                        Thread form = new Thread(() => Application.Run(new FormPrincipal()));
+                        form.Start();   
+                    }
+                    else {
+                        MessageBox.Show("eRRADO");
+                    }
+
+
+                }
+               
+                
+                
+            }catch(SqlException ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                command.Dispose();
+            }
         }
     }
 }
